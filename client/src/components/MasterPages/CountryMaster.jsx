9fd075a36ae5d608836c624b7f8cc741725628e0
vasimaj <jamadarvasim88@gmail.com>
2025-03-14 00:00:16 +0530

    country table che regular insert zale ahe
    duplication che bagne pending ahe

--- client/src/components/MasterPages/CountryMaster.jsx
+++ client/src/components/MasterPages/CountryMaster.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '@/components/shared/pageHeader/PageHeader'
 import PageHeaderDate from '@/components/shared/pageHeader/PageHeaderDate'
 import EmailOverview from '@/components/EmailOverview'
@@ -13,43 +13,103 @@ import MarketingChart from '@/components/widgetsCharts/MarketingChart'
 import Footer from '@/components/shared/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Form, Button, Table, Row, Col } from "react-bootstrap";
+import axios from 'axios'
 
 const CountryMaster = () => {
+    const [CountryCode, setCountryCode] = useState("");
+    const [CountryName, setCountryName] = useState("");
+    const [StatusCode, setStatusCode] = useState(0);
+    const [CountryId, setCountryId] = useState('');
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert("Country Added Successfully")
+
+        // axios.post('http://localhost:8080/api/mihms/add_country', {
+        //     CountryCode: CountryCode,
+        //     CountryName: CountryName,
+        //     StatusCode: StatusCode
+        // })
+        //     .then(result => {
+        //         console.log(result);
+        //         alert("Country Added Successfully");
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
+
+        const dataObj = {
+            CountryCode: CountryCode,
+            CountryName: CountryName,
+            StatusCode: StatusCode,
+            CreatedById: 1,
+            CreatedDate: new Date()
+
+        }
+        if (CountryId == '') {
+            axios.post('http://localhost:8080/api/mihms/add_country', dataObj)
+                .then(result => {
+                    console.log(result);
+                    alert("Country Added Successfully");
+                    window.location.reload();
+
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+
+
     }
+
+
     return (
         // <div>CountryMaster</div>
         <>
-            <PageHeader >
+            <PageHeader>
                 <PageHeaderDate />
             </PageHeader>
             <Container className="mt-4">
                 {/* <h3 className="text-center">Country Master</h3> */}
                 <Form>
                     <Row className="align-items-center">
-                        <Col md={6}>
+                        <Col md={4}>
                             <Form.Group>
                                 <Form.Label>Country Code</Form.Label>
                                 <Form.Control
                                     type="text"
                                     placeholder="Enter Code"
+                                    name='CountryCode'
+                                    onChange={(e) => setCountryCode(e.target.value)}
                                 // value={countryCode}
                                 // onChange={(e) => setCountryCode(e.target.value)}
                                 />
                             </Form.Group>
                         </Col>
-                        <Col md={6}>
+                        <Col md={4}>
                             <Form.Group>
                                 <Form.Label>Country Name</Form.Label>
                                 <Form.Control
                                     type="text"
                                     placeholder="Enter Country Name"
+                                    name='CountryName'
+                                    onChange={(e) => setCountryName(e.target.value)}
                                 // value={countryName}
                                 // onChange={(e) => setCountryName(e.target.value)}
                                 />
+                            </Form.Group>
+                        </Col>
+                        <Col md={4}>
+                            <Form.Group>
+                                <Form.Label>Select Status</Form.Label>
+                                <Form.Select
+                                    aria-label="Default select example"
+                                    value={StatusCode}
+                                    onChange={(e) => setStatusCode(Number(e.target.value))}
+                                >
+                                    <option value="1">Active</option>
+                                    <option value="0">Inactive</option>
+                                </Form.Select>
                             </Form.Group>
                         </Col>
 
